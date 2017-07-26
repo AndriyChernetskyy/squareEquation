3447f87edc802980fc88a3efd6d3de66e82b5b95
Andriy Chernetskyy <andrewusa921@gmail.com>
2017-07-26 13:39:35 +0000

    rewrite

--- js/index.js
+++ js/index.js
@@ -1,24 +1,26 @@
-let a = Number(prompt('Enter koef near x^2'));
-let b = Number(prompt('Enter koef near x'));
-let c = Number(prompt('Enter free member'));
+ let a = Number(prompt('Enter koef near x^2'));
+    let b = Number(prompt('Enter koef near x'));
+    let c = Number(prompt('Enter free member'));
 function diskriminant(a,b,c) {
-    let d = Math.pow(b,2) - (4*(a*c));
-    return d;
+    let r = Math.pow(b,2) - (4*(a*c));
+    return r;
 }
-let r = diskriminant(a,b,c);
-if(r < 0) {
-     document.write('There is no result');
-}
-else {
-    if(r === 0) {
-    let result = 0 - (b/(2*a));
-    document.write('The result is only one, so x= '+result);
+function solving(f,k,l) {
+    let diskr = diskriminant(a,b,c);
+    if(diskr < 0) {
+        return 'There is no result';
     }
     else {
-        let result1 = (-b - Math.sqrt(r))/(2*a);
-        let result2 = (-b + Math.sqrt(r))/(2*a);
-        document.write('The result is x1= '+result1+"<br>");
-        document.write('x2= '+result2);
+        if(diskr === 0) {
+            let result = 0 - (b/(2*a));
+            return('The result is only one, so x= '+result);
+            }
+    else {
+        let result1 = (-b - Math.sqrt(diskr))/(2*a);
+        let result2 = (-b + Math.sqrt(diskr))/(2*a);
+        return('The result is x1= '+result1+"<br>"+'x2= '+result2);
     }
     
 }
+}
+document.write(solving(a,b,c));
